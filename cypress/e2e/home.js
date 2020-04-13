a0eb80290a9b18cd1b3419c00515e8d76fd084bb
Joshua Harris <joshua.jharris13@gmail.com>
2020-04-13 13:04:13 -0400

    Updates cypress test.

--- cypress/e2e/home.js
+++ cypress/e2e/home.js
@@ -1,5 +1,6 @@
 describe('homepage', () => {
   it('learn react link is present', () => {
+    cy.visit(`/`);
     cy.location('pathname').should('include', '/');
     cy.findByText(/^learn react$/i);
   });
